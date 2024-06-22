f2a505aa350f86e8daba773c1e140ba5fb43fcd6
ASSET <asset@MacBook-Pro-ASSET.local>
2024-06-22 23:30:12 +0200

    asset

---
+++ app.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter, Request
+
+from openai_apis import text_to_speech, transcript_audio, chat_completion
+from telegram_api import send_audio, send_message, set_webhook, get_file_path, save_file_and_get_local_path
+from utils import upload_file_to_gcs
+import config
+
+{
+"secret_token": "2f6OpVEPB9nnhYLvrvgEWUp6fMy_3eNqobLfoSiapSgRSR73r",
+"url": "https://843a1a6f02ce.ngrok.ap/telegram"
+}
+
+router = APIRouter(
+    prefix='',
+    responses={404: {'description': 'Not found'}}
+)
+
+
+@router.get('/')
+def home():
+    return 'OK', 200
+
+
+@router.post('/telegram')
+async def telegram(request: Request):
+    try:
+        body = await request.json()
+        print(body)
+        sender_id = body['message']['from']['id']
+        if 'voice' in body['message'].keys():
+            file_id = body['message']['voice']['file_id']
+            file_path = get_file_path(file_id)
+            if file_path['status'] == 1:
+                local_file_path = save_file_and_get_local_path(
+                    file_path['file_path'])
+                if local_file_path['status'] == 1:
+                    transcript = transcript_audio(
+                        local_file_path['local_file_path'], local_file_path['file_id'])
+                    if transcript['status'] == 1:
+                        query = chat_completion(transcript['transcript'])
+        else:
+            query = body['message']['text']
+        response = chat_completion(query)
+        """Convert this response to audio using the following function
+        audio_file_path = text_to_speech(response)
+        Then upload this audio_file to a cloud storage and get the public URL
+        audio_url = upload_file(audio_file_path)
+        you need to write the function upload_file
+        send_audio(audio_url)
+        """
+        if config.REPLY_TYPE == 'audio':
+            audio_file_path, audio_file_name = text_to_speech(response)
+            public_url = upload_file_to_gcs(audio_file_path, audio_file_name)
+            send_audio(sender_id, public_url, 'Response')
+        else:
+            send_message(sender_id, response)
+        return 'OK', 200
+    except Exception as e:
+        print('Error at telegram...')
+        print(e)
+        return 'OK', 200
+
+
+@router.post('/set-telegram-webhook')
+async def set_telegram_webhook(request: Request):
+    try:
+        """Create and return the webhook URL {BASE_URL}/telegram"""
+        base_url = str(request.base_url)
+        webhook_url = f"{base_url}telegram"
+        set_webhook(webhook_url)
+        return "OK", 200
+    except Exception as e:
+        print('Error at set_telegram_webhook...')
+        print(e)
+        return 'BAD REQUEST', 400
---
+++ openai_apis.py
@@ -0,0 +1,80 @@
+import os
+import tempfile
+import uuid
+
+from openai import OpenAI
+import soundfile as sf
+import config
+
+client = OpenAI(
+    api_key=config.OPENAI_API_KEY
+)
+
+OUTPUT_DIR = os.path.join(
+    tempfile.gettempdir(),
+    'openai_telegram',
+    'audios'
+)
+
+os.makedirs(
+    OUTPUT_DIR,
+    exist_ok=True
+)
+
+
+def transcript_audio(ogg_file_path: str, file_id: str) -> dict:
+    try:
+        audio_data, sample_rate = sf.read(ogg_file_path)
+        mp3_file_path = f'{OUTPUT_DIR}/{file_id}.mp3'
+        sf.write(mp3_file_path, audio_data, sample_rate)
+        audio_file = open(mp3_file_path, 'rb')
+        os.unlink(ogg_file_path)
+        os.unlink(mp3_file_path)
+        transcript = client.audio.transcriptions.create(
+            model='whisper-1',
+            file=audio_file,
+            response_format="text"
+        )
+        return {
+            'status': 1,
+            'transcript': transcript
+        }
+    except Exception as e:
+        print('Error at transcript_audio...')
+        print(e)
+        return {
+            'status': 0,
+            'transcript': ''
+        }
+
+
+def chat_completion(text: str) -> str:
+    try:
+        response = client.chat.completions.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                {'role': 'system', 'content': 'You are a helpful assistant.'},
+                {'role': 'user', 'content': text}
+            ]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print('Error at chat_completion...')
+        print(e)
+        return 'We are facing an issue at this moment.'
+
+
+def text_to_speech(text: str) -> tuple[str, str]:
+    file_name = f'{uuid.uuid1()}.{config.AUDIO_FILE_FORMAT}'
+    audio_file_path = os.path.join(
+        config.OUTPUT_DIR,
+        file_name
+    )
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=text,
+        response_format=config.AUDIO_FILE_FORMAT
+    )
+    response.write_to_file(audio_file_path)
+    return audio_file_path, file_name
